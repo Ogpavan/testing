880170fd74d22d16e8ec3138d1af8238ad7dcc8b
Ogpavan <pawantwp@gmail.com>
2025-07-06 12:02:53 +0530

    asdsad

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>CICD Success</h1>
+      <h1>CICD s</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
